d2f8ac8ee325457ffb497779ddd835b5fd891068
NicholasFarrow <nicholas.w.farrow@gmail.com>
2019-05-13 00:57:21 +1000

    Initial upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#for CFGFOLDER in `find $HOME/plugfiles/.config/ -maxdepth 1`
+
+PLUGLOC=$HOME/plugfiles
+CFGLOC=$HOME/.config
+
+for CFGFOLDER in $PLUGLOC/.config/*/
+do
+  FOLDERNAME=$(basename $CFGFOLDER)
+    
+  OLDDIR=$CFGLOC/$FOLDERNAME/
+  
+  [ -d "$OLDDIR" ] && echo "deleting $OLDDIR" && rm -r $OLDDIR 
+  cp -R "$CFGFOLDER" "$OLDDIR"
+done
+
+[ -f "$CFGLOC/.aliasrc" ] && rm "$CFGLOC/.aliasrc"
+cp "$PLUGLOC/.config/.aliasrc" "$CFGLOC/.aliasrc"
+cp "$PLUGLOC/.config/.compton.conf" "$CFGLOC/.compton.conf"
+
+ln -svf "$PLUGLOC/.bash_profile" ~
+ln -svf "$PLUGLOC/.bash_profile" ~/.profile
+ln -svf "$PLUGLOC/.bashrc" ~
+ln -svf "$PLUGLOC/.inputrc" ~
+
+
+source "$PLUGLOC/.bash_profile"
