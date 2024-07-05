7a9c2ba3e07ec6588f43d47046e22cb83c4efb2c
Nick Farrow <nick@nickfarrow.com>
2024-07-06 00:37:49 +1000

    improve installer and cleanup

--- install.sh
+++ install.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
-#for CFGFOLDER in `find $HOME/plugfiles/.config/ -maxdepth 1`
 
 PLUGLOC=$HOME/plugfiles
 CFGLOC=$HOME/.config
 BINLOC=$HOME/.local/bin
 
-#[ -f $CFGLOC ] || mkdir $CFGLOC
-#[ -f $BINLOC ] || mkdir $BINLOC
-
-mkdir -p $CFGLOC
-mkdir -p $BINLOC
-
-for CFGFOLDER in $PLUGLOC/.config/*/
-do
-  FOLDERNAME=$(basename $CFGFOLDER)
-    
-  OLDDIR=$CFGLOC/$FOLDERNAME/
-  
-  [ -d "$OLDDIR" ] && echo "deleting $OLDDIR" && rm -r $OLDDIR 
-  cp -R "$CFGFOLDER" "$OLDDIR"
+# Copy config folders
+for CFGFOLDER in "$PLUGLOC/.config"/*/; do
+    FOLDERNAME=$(basename "$CFGFOLDER")
+    OLDDIR="$CFGLOC/$FOLDERNAME"
+    if [ -d "$OLDDIR" ]; then
+        echo "Removing $OLDDIR"
+        rm -rf "$OLDDIR"
+    fi
+    echo "Copying $CFGFOLDER to $OLDDIR"
+    cp -R "$CFGFOLDER" "$OLDDIR"
 done
 
-cp -r $PLUGLOC/bin/* $BINLOC/
+# Copy bin files
+echo "Copying bin files"
+cp -r "$PLUGLOC/bin/"* "$BINLOC/"
 
-[ -f "$CFGLOC/.aliasrc" ] && rm "$CFGLOC/.aliasrc"
-cp "$PLUGLOC/.config/.aliasrc" "$CFGLOC/.aliasrc"
-cp "$PLUGLOC/.config/picom.conf" "$CFGLOC/picom.conf"
-cp "$PLUGLOC/.config/emoji" "$CFGLOC/emoji"
-cp "$PLUGLOC/starshell.sh" "$CFGLOC/starshell.sh"
+# Copy config files
+config_files=(".aliasrc" "picom.conf" "emoji")
+for file in "${config_files[@]}"; do
+    if [ -f "$CFGLOC/$file" ]; then
+        echo "Removing old $file"
+        rm "$CFGLOC/$file"
+    fi
+    echo "Copying $file"
+    cp "$PLUGLOC/.config/$file" "$CFGLOC/$file"
+done
+
+echo "Copying starshell directory"
+rm -rf "$CFGLOC/starshell"
 cp -r "$PLUGLOC/.config/starshell" "$CFGLOC/starshell"
 
-ln -svf "$PLUGLOC/.bash_profile" ~
-ln -svf "$PLUGLOC/.bash_profile" ~/.profile
-ln -svf "$PLUGLOC/.bashrc" ~
-ln -svf "$PLUGLOC/.inputrc" ~
+# Create symlinks
+symlink_files=(".bash_profile" ".bashrc" ".inputrc")
+for file in "${symlink_files[@]}"; do
+    echo "Creating symlink for $file"
+    ln -svf "$PLUGLOC/$file" "$HOME/$file"
+done
 
-
-source "$PLUGLOC/.bash_profile"
+ln -svf "$PLUGLOC/.bash_profile" "$HOME/.profile"
+echo "Done! Restart your shell!"
